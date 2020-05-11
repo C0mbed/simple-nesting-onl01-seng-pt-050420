e597a587e4121b8d2cc981e61a4e8fd4e6ce1696
Matt Farncombe <matt@farncombe.co>
2020-05-11 12:52:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  return programmer_hash[:dennis_ritchie][:languages].str
+     answer = programmer_hash[:dennis_ritchie][:languages]
+     return answer
 end
 
 def adding_matz
